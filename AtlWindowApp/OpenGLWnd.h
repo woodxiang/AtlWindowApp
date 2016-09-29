06da16b384dab68002f83a751fc44ee8bbf92e73
dongx <dongx@DESKTOP-03KPNNG>
2016-09-29 22:49:09 +0800

    1

---
+++ AtlWindowApp/OpenGLWnd.h
@@ -0,0 +1,130 @@
+//=============================================================================
+//
+// SHICHUANG CONFIDENTIAL
+// __________________
+//
+//  [2016] - [2016] SHICHUANG Co., Ltd.
+//  All Rights Reserved.
+//
+//=============================================================================
+
+#pragma once
+
+template <class CRender, class TWinTraits = CControlWinTraits>
+class COpenGLWnd :
+	public CWindowImpl<COpenGLWnd<CRender>, CWindow, TWinTraits>
+{
+public:
+	DECLARE_WND_CLASS_EX(_T("MainWindow"), CS_HREDRAW | CS_VREDRAW, (HBRUSH)(COLOR_WINDOW + 1))
+
+	BEGIN_MSG_MAP(CMainWindow)
+		MESSAGE_HANDLER(WM_CREATE, OnCreate)
+		MESSAGE_HANDLER(WM_PAINT, OnPaint)
+		MESSAGE_HANDLER(WM_SIZE, OnSize)
+		MESSAGE_HANDLER(WM_SIZING, OnSizing)
+		MESSAGE_HANDLER(WM_CLOSE, OnClose)
+		MESSAGE_HANDLER(WM_MOUSEWHEEL, OnMouseWheel)
+		MESSAGE_HANDLER(WM_LBUTTONDOWN, OnMouseLeftButtonDown)
+		MESSAGE_HANDLER(WM_MOUSEMOVE, OnMouseMove)
+		MESSAGE_HANDLER(WM_LBUTTONUP, OnMouseLeftButtonUp)
+	END_MSG_MAP()
+
+	LRESULT OnCreate(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		m_Render.Init(m_hWnd);
+		return S_OK;
+	}
+
+	LRESULT OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		PAINTSTRUCT ps;
+		BeginPaint(&ps);
+		EndPaint(&ps);
+		return S_OK;
+	}
+
+	HRESULT OnSize(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		m_Render.Resize(LOWORD(lParam), HIWORD(lParam));
+		return S_OK;
+	}
+
+	HRESULT OnSizing(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		m_Render.Resize(LOWORD(lParam), HIWORD(lParam));
+		return S_OK;
+	}
+
+	LRESULT OnClose(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		m_Render.Free();
+		DestroyWindow();
+		return S_OK;
+	}
+
+	LRESULT OnMouseWheel(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		auto zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+		m_Render.Zoom(zDelta);
+		return S_OK;
+	}
+
+	LRESULT OnMouseLeftButtonDown(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		::SetCapture(m_hWnd);
+
+		if (m_hOldCursor == 0)
+		{
+			m_hOldCursor = SetCursor(LoadCursor(NULL, IDC_HAND));
+		}
+
+		m_iLastXPosition = GET_X_LPARAM(lParam);
+		m_iLastYPosition = GET_Y_LPARAM(lParam);
+
+		m_bIsDragoning = true;
+
+		return S_OK;
+	}
+	LRESULT OnMouseLeftButtonUp(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		m_bIsDragoning = false;
+		if (m_hOldCursor != 0)
+		{
+			SetCursor(m_hOldCursor);
+			m_hOldCursor = 0;
+		}
+
+		::ReleaseCapture();
+		return S_OK;
+	}
+	LRESULT OnMouseMove(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		if (m_bIsDragoning)
+		{
+			m_Render.Move(GET_X_LPARAM(lParam) - m_iLastXPosition, GET_Y_LPARAM(lParam) - m_iLastYPosition);
+			m_iLastXPosition = GET_X_LPARAM(lParam);
+			m_iLastYPosition = GET_Y_LPARAM(lParam);
+		}
+		return S_OK;
+	}
+
+	void OnFinalMessage(HWND hWnd)
+	{
+		PostQuitMessage(0);
+	}
+
+	void Render()
+	{
+		m_Render.Render();
+	}
+
+	CRender* GetRender() { return &m_Render; }
+
+private:
+	CRender m_Render;
+
+	bool m_bIsDragoning;
+	HCURSOR m_hOldCursor;
+	int m_iLastXPosition = -1;
+	int m_iLastYPosition = -1;
+};
